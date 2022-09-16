7713f470b8da66584ba45c736a0f52c727ff5884
Snoopy1sassy <anjalinand0912@gmail.com>
2022-09-16 13:56:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundColor = "lightcyan"
+document.getElementById("h1") .innerHTML = "Hii! This is my Self Introduction"
+document.getElementById("h1") .style.color = "Blue"
+document.getElementById("Self Introduction") .innerHTML = "Hii! My name is Anjelina. I am a Web Designer by Profession. It has been three years since I have been in this profession and I enjoy my work a lot."
+document.getElementById("h3") .innerHTML = "What does a web designer do?"
+document.getElementById("p1") .innerHTML = "Web designers identifies the goals of a website or webpage and promotes accessibility for all potential users. This process involves organizing content and images across a series of pages, integrating applications and other interactive elements."
+document.getElementById('Self Introduction') .style.fontSize = '30px';
+document.getElementById("h3") .style.color = "crimson"
+document.getElementById("p1") .style.fontSize = '30px';
+
+
